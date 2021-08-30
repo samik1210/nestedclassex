07c4979dff407b81f84423aaa3bef02a7c9ba67a
parag <parag@DESKTOP-I2Q9QKQ>
2021-08-30 17:40:32 +0530

    Add project files.

---
+++ nestedclassex/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace nestedclassex
+{
+    public class Outer_class
+    {
+        public void method1()
+        {
+            Console.WriteLine("Outer class method");
+        }
+
+
+        public class Inner_class
+        {
+
+
+            public void method2()
+            {
+                Console.WriteLine("Inner class Method");
+            }
+        }
+    }
+
+
+    public class G1
+    {
+
+
+        static public void Main()
+        {
+
+
+            Outer_class obj1 = new Outer_class();
+            obj1.method1();
+
+            Outer_class.Inner_class obj2 =
+                              new Outer_class.Inner_class();
+
+            obj2.method2();
+        }
+    }
+}
